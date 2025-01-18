26a2610d6b2b425af4359f04ac6500b1c7c287f7
ntando-mv15 <ntando.mv15@gmail.com>
2025-01-18 14:29:48 +0200

    Modified logging and created main function

--- cleanup_script/src/cleanup.py
+++ cleanup_script/src/cleanup.py
@@ -4,29 +4,28 @@ import logging
 log_dir = "/mnt/c/Users/NTAND/cleanup_project/logs" 
 log_file = os.path.join(log_dir, "script.log")
 
+# Setup logging
+os.makedirs(log_dir, exist_ok=True)  # Ensure logs directory exists
+logging.basicConfig(filename=log_file, level=logging.INFO)
+
 
 # Create .txt and .tmp files 
 def create_file(directory, name, count, file_ext):
+    # Create files in specific directory
     for number in range(1, count + 1):
         filename = f"{name}{number}{file_ext}"
-        if not os.path.exists(os.path.join(directory, filename)):
-            with open(os.path.join(directory, filename), "w") as newfile:
+        file_path = os.path.join(directory, filename)
+        if not os.path.exists(file_path):
+            with open(file_path, "w") as newfile:
                 pass
             print(f"File {filename} successfully created")
         else:
             print(f"File {filename} already exists")
             
-            
-    
-list_dir = os.listdir(log_dir)
-print("Before cleanup:", list_dir) 
-
-# Set up logging 
-logging.basicConfig(filename=log_file, level=logging.INFO)
 
 
 def cleanup_tmp_files():
-    # Delete .tmp files in specified directory
+    # Delete .tmp files in specific directory
     for file in os.listdir(log_dir):
         if file.endswith(".tmp"):
             file_path = os.path.join(log_dir, file)
@@ -38,18 +37,24 @@ def cleanup_tmp_files():
                 logging.info(f"Error deleting file {file_path}: {e}")
                 print(f"Error deleting file {file_path}: {e}")
 
-list_dir = os.listdir(log_dir)
-print("After cleanup:", list_dir)
+
 
 def main():
+    print(f"Logs directory: {log_dir}")
+    
+    # Create files
     create_file(log_dir, "temp", 6, ".tmp")
     create_file(log_dir, "log", 3, ".txt")
     
+    list_dir = os.listdir(log_dir)
+    print("Before cleanup:", list_dir)
     
+    # Cleanup .tmp files
+    cleanup_tmp_files()
     
-    
-    
-    
+    # List files before cleanup
+    list_dir = os.listdir(log_dir)
+    print("After cleanup:", list_dir) 
 
 if __name__ == "__main__":
     main()
